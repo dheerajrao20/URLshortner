4bb7a029eeb5f0c191db418f8b6febc4c5a025ab
DHEERAJ KUMAR <dheerajrao20@outlook.com>
2022-06-07 00:22:20 +0530

    sdc

--- URLshortner/settings.py
+++ URLshortner/settings.py
@@ -15,6 +15,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 # Quick-start development settings - unsuitable for production
@@ -28,7 +29,10 @@ SECRET_KEY = 'django-insecure-f4*4cx)x(l&^k0y+(#$ufnt%)ysnowm9cyxjt+10e)ev9ojl!@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'easyurlshortnerhere.herokuapp.com',
+    '127.0.0.1'
+]
 
 
 # Application definition
